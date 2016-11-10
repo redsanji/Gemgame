d824247ca8785986b8fa51e929f80ec8febfa85c
Administrator <Administrator@EOF-01507201721>
2016-11-11 01:51:22 +0700

    มีเสียงประกอบแล้ว

--- core/src/com/sak/gdx/game/GemGame.java
+++ core/src/com/sak/gdx/game/GemGame.java
@@ -1,18 +1,25 @@
 package com.sak.gdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GemGame extends Game {
     public SpriteBatch batch;
-    public static final int HEIGHT = 600;
+    public static final int HEIGHT = 520;
     public static final int WIDTH = 800;
+    private Music music;
     
  
     @Override
     public void create () {
         batch = new SpriteBatch();
         setScreen(new GameScreen(this));
+        music = Gdx.audio.newMusic(Gdx.files.internal("Realm's Awakening - Valkyrie Crusade Music Extended.mp3"));
+        music.setLooping(true);
+        music.setVolume(0.2f);
+        music.play();
     }
  
     @Override
@@ -23,5 +30,7 @@ public class GemGame extends Game {
     @Override
     public void dispose () {
         batch.dispose();
+        music.dispose();
     }
+    
 }
